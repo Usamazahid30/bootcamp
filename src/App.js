2428f15aaae5051536cefc835885c05cadcd3624
usama_zahid <Usamazahid30@users.github.com>
2021-01-10 23:33:40 +0500

    update some info

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
     <h1>hello world</h1>
     <h1>hello world</h1>
-      </header>
+    <h2>hello world</h2>
+    <h3>hello world</h3>
+    <h4>helo world</h4>
     </div>
   );
 }
