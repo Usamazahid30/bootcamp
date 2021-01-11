c2d9664c4f46fd0dd3fb60684d72fe10af9f66a8
usama_zahid <Usamazahid30@users.github.com>
2021-01-11 19:13:24 +0500

    remove h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
     <h1>hello world</h1>
-    <h1>hello world</h1>
     <h2>hello world</h2>
     <h3>hello world</h3>
     <h4>helo world</h4>
