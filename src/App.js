6700929b9d2c4e1298c9aae97d6185cd7e078aea
usama_zahid <Usamazahid30@users.github.com>
2021-02-02 11:31:17 +0500

    changing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <h1>hello world</h1>
-    <h2>hello world</h2>
-    <h3>hello world</h3>
-    <h4>helo world</h4>
+    <p>Hi! Welcome here</p>
     </div>
   );
 }
